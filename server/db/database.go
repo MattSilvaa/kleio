8635b3c28262f55e74022ace7103ff712b218e61
Matheus Muniz da Silva <20505926+MattSilvaa@users.noreply.github.com>
2023-09-24 15:19:37 -0700

    .

--- server/db/database.go
+++ server/db/database.go
@@ -1,8 +1,28 @@
-package db
+package database
 
 import "database/sql"
 
-func connectToDB() (*sql.DB, error) {
+func create() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "username:password@tcp(host:port)/database-name")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connect() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "username:password@tcp(host:port)/database-name")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func delete() (*sql.DB, error) {
 	db, err := sql.Open("mysql", "username:password@tcp(host:port)/database-name")
 
 	if err != nil {
--- server/main.go
+++ server/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	database "github.com/MattSilvaa/kleio/server/database"
 	"log"
 )
 
 func main() {
-	db, err := connectToDB()
+	db, err := database.connect()
 	if err != nil {
 		log.Fatalf("Error connecting to the database:\t%v\n", err)
 	}
