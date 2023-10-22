aaf497eac50f56ec63f6953839d6e6a331dca518
MattSilvaa <mattsilvaa@gmail.com>
2023-10-22 10:01:39 -0700

    setting up server handling routines

--- server/main.go
+++ server/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/MattSilvaa/kleio/server/database"
-	"github.com/MattSilvaa/kleio/server/models"
+	"github.com/MattSilvaa/kleio/server/routes"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -23,5 +29,41 @@ func main() {
 		log.Fatalf("Failed to create table: %v\n", err)
 	}
 
-	models.UploadJobCount()
+	router := routes.SetupRouter()
+
+	// Create a server and set timeout values
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  25 * time.Second,
+		WriteTimeout: 25 * time.Second,
+	}
+
+	// Start the server in a goroutine
+	go func() {
+		log.Println("Server listening on port 8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe failed: %v\n", err)
+		}
+	}()
+
+	// Channel to listen for signals
+	sigChan := make(chan os.Signal, 1)
+	// Notify the sigChan on signals
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive the signal
+	sigReceived := <-sigChan
+	log.Printf("Received signal: %v. Shutting down...\n", sigReceived)
+
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
+	defer cancel()
+
+	// Gracefully shutdown the server
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
+
+	log.Println("Server stopped gracefully.")
 }
